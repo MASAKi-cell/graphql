843d86dd72379ca3778e3e7d349d4d0ca74055c0
masaki.takahashi <masaki.takahashi@milab-inc.com>
2022-06-30 10:06:02 +0900

    Running an Express GraphQL Server

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 const express = require('express');
 const { GraphQLHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+  type Query {
+      hello: String
+  }
+`);
+
+const root = {
+    hello: () => {
+        return 'Hello world'
+    }
+}
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
