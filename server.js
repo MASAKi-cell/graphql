c2dce541e1736b3dbd5a9b5a1edbfe1ee619ae1b
masaki.takahashi <masaki.takahashi@milab-inc.com>
2022-07-01 10:07:53 +0900

    Queryの追加

--- server.js
+++ server.js
@@ -5,12 +5,28 @@ const { buildSchema } = require('graphql');
 const schema = buildSchema(`
   type Query {
       hello: String
+      quoteOfTheDay: String
+      random: Float!
+      rollDice(numDice: Int!, numSides: Int):[Int]
   }
 `);
 
 const root = {
     hello: () => {
         return 'Hello world'
+    },
+    quoteOfTheDay: () => {
+      return Math.random() < 0.5 ? 'Take it easy' : 'Save Something'
+    },
+    random: () => {
+      return Math.random();
+    },
+    rollDice: ({numDice, numSides}) => {
+      const output = [];
+      for(let i = 0; i < numDice; i++) {
+        output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+      }
+      return output;
     }
 }
 
