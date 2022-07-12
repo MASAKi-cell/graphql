a32706140845f14e4f4d84db143860a95fae9071
masaki.takahashi <bje35149@gmail.com>
2022-07-12 15:45:42 +0900

    node_modulesの整理

--- apollo-client/src/main.js
+++ apollo-client/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// import { createProvider } from './vue-apllo'
+// import { vuetify } from './pugins/vuetify'
 
 createApp(App).mount('#app')
